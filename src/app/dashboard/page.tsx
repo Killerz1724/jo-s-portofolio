c72d379facc997ee53860e63cb176ccc9fa41ae8
tejo <tejo.hananto@shopee.com>
2025-08-02 14:31:11 +0700

    feat: added animation scroll on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,16 +2,22 @@ import clsx from "clsx";
 import Headline from "./Headline";
 import Skills from "./Skills";
 import Career from "./Career";
-
+import AnimateOnScroll from "@/lib/AnimateOnScroll";
 
 const Dashboard = () => {
-    return (
-        <div className={clsx("w-full flex flex-col items-center gap-[10rem]")}>
-            <Headline />
-            <Skills />
-            <Career />
-        </div>
-    );
+  return (
+    <div className={clsx("w-full flex flex-col items-center gap-[10rem]")}>
+      <AnimateOnScroll>
+        <Headline />
+      </AnimateOnScroll>
+      <AnimateOnScroll>
+        <Skills />
+      </AnimateOnScroll>
+      <AnimateOnScroll>
+        <Career />
+      </AnimateOnScroll>
+    </div>
+  );
 };
 
 export default Dashboard;
